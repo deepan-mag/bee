90063518f4bb85b298ba10035163d79d5d78aceb
global user <deepanmotog@gmail.com>
2021-04-03 17:39:12 +0530

    Like feature

--- client/src/app/_interceptors/error.interceptor.ts
+++ client/src/app/_interceptors/error.interceptor.ts
@@ -3,45 +3,51 @@ import {
   HttpRequest,
   HttpHandler,
   HttpEvent,
-  HttpInterceptor
+  HttpInterceptor,
 } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
-import { NavigationExtras, Router } from "@angular/router";
-import { ToastrService } from "ngx-toastr";
-import { catchError } from "rxjs/operators";
+import { NavigationExtras, Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { catchError } from 'rxjs/operators';
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
+  constructor(private router: Router, private toastr: ToastrService) {}
 
-  constructor(private router:Router, private toastr:ToastrService) {}
-
-  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+  intercept(
+    request: HttpRequest<unknown>,
+    next: HttpHandler
+  ): Observable<HttpEvent<unknown>> {
     return next.handle(request).pipe(
-      catchError(error =>{
-        if(error){
+      catchError((error) => {
+        if (error) {
           switch (error.status) {
             case 400:
-              if(error.error.errors){
+              if (error.error.errors) {
                 const modalStateErrors = [];
-                for(const key in error.error.errors){
-                  if(error.error.errors[key]){
-                    modalStateErrors.push(error.error.errors[key])
+                for (const key in error.error.errors) {
+                  if (error.error.errors[key]) {
+                    modalStateErrors.push(error.error.errors[key]);
                   }
                 }
                 throw modalStateErrors.flat();
+              } else if (typeof(error.error) === 'object') {
+                this.toastr.error('Bad Request', error.status);
               } else {
-                this.toastr.error("Bad Request", error.status);
+                this.toastr.error(error.error, error.status);
               }
               break;
             case 401:
-              this.toastr.error("Unauthorized", error.status);
+              this.toastr.error('Unauthorized', error.status);
               break;
             case 404:
               this.router.navigateByUrl('/not-found');
               break;
             case 500:
-              const navigationExtras: NavigationExtras = {state:{error:error.error}};
-              this.router.navigateByUrl('/server-error',navigationExtras);
+              const navigationExtras: NavigationExtras = {
+                state: { error: error.error },
+              };
+              this.router.navigateByUrl('/server-error', navigationExtras);
               break;
             default:
               this.toastr.error('Something unexpected went wrong');
@@ -51,6 +57,6 @@ export class ErrorInterceptor implements HttpInterceptor {
         }
         return throwError(error);
       })
-    )
+    );
   }
 }
